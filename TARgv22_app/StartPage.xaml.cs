494d4e9f522d99a937a89db2858a1d2503cb6427
RainonC <118743359+RainonC@users.noreply.github.com>
2023-11-20 12:59:01 +0200

    init

--- TARgv22_app/StartPage.xaml.cs
+++ TARgv22_app/StartPage.xaml.cs
@@ -26,14 +26,38 @@ namespace TARgv22_app
                 BackgroundColor = Color.Azure
             };
 
+            Button Fr_grid_btn = new Button
+            {
+                Text = "Frame Grid",
+                BackgroundColor = Color.Azure
+            };
+
+            Button Img_btn = new Button
+            {
+                Text = "Image",
+                BackgroundColor = Color.Azure
+            };
+
             StackLayout st = new StackLayout
             {
-                Children = { Ent_btn, Time_btn },
+                Children = { Ent_btn, Time_btn, Fr_grid_btn, Img_btn },
                 BackgroundColor = Color.Beige
             };
             Content = st;
             Ent_btn.Clicked += Ent_btn_Clicked;
             Time_btn.Clicked += Time_btn_Clicked;
+            Fr_grid_btn.Clicked += Fr_grid_btn_Clicked;
+            Img_btn.Clicked += Img_btn_Clicked;
+        }
+
+        private async void Img_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ImagePage());
+        }
+
+        private async void Fr_grid_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FrameGridPage());
         }
 
         private async void Ent_btn_Clicked(object sender, EventArgs e)
